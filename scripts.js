2b5237248978f441ab0ae78815ea66e4125b7011
Christina Varghese <46170618+christinichka@users.noreply.github.com>
2021-03-30 20:12:46 -0500

    Update scripts.js
    Updated shuttleHeight

--- scripts.js
+++ scripts.js
@@ -4,16 +4,20 @@ const landing = document.getElementById("landing");
 const missionAbort = document.getElementById("missionAbort");
 const flightStatus = document.getElementById("flightStatus");
 const shuttleBackground = document.getElementById("shuttleBackground");
-const spaceShuttleHeight = document.getElementById("spaceShuttleHeight");
+const spaceShuttleHeight = document.getElementById("spaceShuttleHeight"); 
 const upButton = document.getElementById("up");
 const downButton = document.getElementById("down");
 const leftButton = document.getElementById("left");
 const rightButton = document.getElementById("right");
 const rocket = document.getElementById("rocket");
 
+
 let xPosition = 350;
 let yPosition = 250;
-let shuttleAltitude = [];
+
+
+const score = spaceShuttleHeight.innerHTML;
+let count = Number(score);
 
 // console.log(originalPosition);
 
@@ -22,7 +26,8 @@ function shuttleInFlight(){
     window.confirm("Confirm that the shuttle is ready for takeoff.");
     flightStatus.innerHTML ="Shuttle in flight.";
     shuttleBackground.style.backgroundColor = "blue";
-    spaceShuttleHeight.innerHTML = 10000;
+    spaceShuttleHeight.innerHTML = count + 10000;
+    rocket.style.top = (yPosition -=10) + "px"; /*move rocket up 1*/
 }
 takeoff.addEventListener("click", shuttleInFlight);
 // Landing Button Events
@@ -32,13 +37,10 @@ function shuttleHasLanded(){
     shuttleBackground.style.backgroundColor = "green";
     spaceShuttleHeight.innerHTML = 0;
      /*go back to original position*/
-     if (xPosition != 350 && yPosition != 250) {
-        rocket.style.left;
-        rocket.style.top;
-        
-    }
-     
+    //  rocket.style.left = (xPosition) + 'px';
+    //  rocket.style.top = (yPosition) + "px";
 }
+     
 landing.addEventListener("click", shuttleHasLanded);
 // Mission Aborted Button Events
 function missionAborted(){
@@ -55,8 +57,7 @@ missionAbort.addEventListener("click", missionAborted);
 // Move rocket using the 4 directional buttons
 function moveRight() {
     if (xPosition < 700) {
-        rocket.style.left = (xPosition +=10) + 'px';
-        
+        rocket.style.left = (xPosition +=10) + 'px'; 
     }
 }
 rightButton.addEventListener("click", moveRight);
@@ -73,7 +74,7 @@ leftButton.addEventListener("click", moveLeft);
 function moveUp() {
     if (yPosition > 0 && yPosition < 400) {
     rocket.style.top = (yPosition -=10) + "px";
-    spaceShuttleHeight.innerHTML = 10000; /*+10000 miles every time the button is clicked*/
+    spaceShuttleHeight.innerHTML = count += 10000; /*+10000 miles every time the button is clicked*/
     }
 }
 upButton.addEventListener("click", moveUp);
@@ -81,7 +82,12 @@ upButton.addEventListener("click", moveUp);
 function moveDown() {
     if (yPosition < 250) {
     rocket.style.top = (yPosition +=10) + "px";
-    spaceShuttleHeight.innerHTML = -10000; /*-10000 miles every time the button is clicked*/
+        if (count > 0) {
+            spaceShuttleHeight.innerHTML = count -= 10000; /*-10000 miles every time the button is clicked*/
+        } else {
+            spaceShuttleHeight.innerHTML = count;
+        }
+    
     }
 }
 downButton.addEventListener("click", moveDown);
@@ -89,34 +95,3 @@ downButton.addEventListener("click", moveDown);
 }
 // Page loading
 window.addEventListener("load", init);
-
-
-
-
-//Original moving code
-// function moveRight() {
-//     position +=10;
-//     rocket.style.left = position + 'px';
-// }
-// rightButton.addEventListener("click", moveRight);
-
-// function moveLeft() {
-//     position -=10;
-//     rocket.style.left = position + 'px';
-// }
-// leftButton.addEventListener("click", moveLeft);
-
-// function moveUp() {
-//     position -=10;
-//     rocket.style.top = position + "px";
-//     spaceShuttleHeight.innerHTML = 10000; /*+10000 miles every time the button is clicked*/
-// }
-// upButton.addEventListener("click", moveUp);
-
-// function moveDown() {
-//     position +=10;
-//     rocket.style.top = position + "px";
-//     spaceShuttleHeight.innerHTML = -10000; /*-10000 miles every time the button is clicked*/
-// }
-// downButton.addEventListener("click", moveDown);
-
